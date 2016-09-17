6cf7f31eaf42c2960b3cfb85e2bf391265bf698a
Jinwoo Kim <lastkuku@gmail.com>
2016-09-17 11:10:47 +0900

    [update] - 입력된 숫자에 의한 리스트 갱신 처리

--- app/src/main/java/yyy/xxx/listviewsample01/MainActivity.java
+++ app/src/main/java/yyy/xxx/listviewsample01/MainActivity.java
@@ -3,6 +3,9 @@ package yyy.xxx.listviewsample01;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -37,33 +40,79 @@ public class MainActivity extends AppCompatActivity {
 	 */
 	private void setView() {
 		setListView();
-//		setApplyButton();
+		setApplyButton();
 	}
 
 
 	/**
 	 * 리스트뷰 초기 세팅 및 리스트뷰와 아답타 연동
 	 */
-	private void setListView(){
+	private void setListView() {
 		listView = (ListView) findViewById(R.id.list_view);
 		listView.setCacheColorHint(0);
-		listView.setDivider(null);
-		listView.setDividerHeight(0);
-		listView.setSelector(null);
 
 		listAdapter = new MultiplicationListAdapter(context, multipleArrayList);
 		listView.setAdapter(listAdapter);
 	}
 
 
-	private void setInputTextLayout(){
+	/**
+	 * 적용 버튼 눌렀을때의 동작 정의
+	 */
+	private void setApplyButton() {
+		final EditText multiplicandInputField = (EditText) findViewById(R.id.multiplicand_input_field);
+		final EditText multiplierInputField = (EditText) findViewById(R.id.multiplier_input_field);
 
+
+		findViewById(R.id.apply_button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String multiplicandInputText = multiplicandInputField.getText().toString();
+				String multiplierInputText = multiplierInputField.getText().toString();
+
+				if (!multiplicandInputText.isEmpty() && !multiplierInputText.isEmpty() && Integer.parseInt(multiplicandInputText) > 0 && Integer.parseInt(multiplierInputText) > 0) {
+					ArrayList<Multiple> resultList = makeMultipleArrayList(Integer.parseInt(multiplicandInputText), Integer.parseInt(multiplierInputText));
+					multipleArrayList.clear();
+					multipleArrayList.addAll(resultList);
+					listAdapter.notifyDataSetChanged();
+
+					hideKeyboard();
+				}
+			}
+		});
 	}
 
 
+	/**
+	 * 곱셈 객체가 들어있는 arraylist를 return 하는 함수
+	 *
+	 * @param multiplicand
+	 * @param multiplier
+	 * @return 곱셈 객체 list
+	 */
+	private ArrayList<Multiple> makeMultipleArrayList(int multiplicand, int multiplier) {
+		ArrayList<Multiple> dataList = new ArrayList<>();
+
+		for (int i = 0; i < multiplier; i++) {
+			Multiple multiple = new Multiple();
+			multiple.setMultiplicand(multiplicand);
+			multiple.setMultiplier(i + 1);
+			multiple.setResult(multiplicand * (i + 1));
+			dataList.add(multiple);
+		}
+
+		return dataList;
+	}
 
 
-
-
-
+	/**
+	 * 키보드 내리는 유틸리티 함수
+	 */
+	private void hideKeyboard() {
+		View view = getCurrentFocus();
+		if (view != null) {
+			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+		}
+	}
 }
--- app/src/main/java/yyy/xxx/listviewsample01/MultiplicationListAdapter.java
+++ app/src/main/java/yyy/xxx/listviewsample01/MultiplicationListAdapter.java
@@ -1,9 +1,11 @@
 package yyy.xxx.listviewsample01;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,6 +21,10 @@ public class MultiplicationListAdapter extends BaseAdapter {
 		this.dataList = dataList;
 	}
 
+	/**
+	 * 리스트 토탈 카운트
+	 * @return
+	 */
 	@Override
 	public int getCount() {
 		return dataList.size();
@@ -34,11 +40,25 @@ public class MultiplicationListAdapter extends BaseAdapter {
 		return position;
 	}
 
+	/**
+	 * 리스트 아이템의 뷰 정의
+	 *
+	 * @param position
+	 * @param convertView
+	 * @param parent
+	 * @return
+	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater layoutInflater = LayoutInflater.from(context);
+		View view = layoutInflater.inflate(R.layout.view_list_item, null);
+		Multiple multiple = dataList.get(position);
 
+		((TextView) view.findViewById(R.id.multiplicand_text)).setText(String.valueOf(multiple.getMultiplicand()));
+		((TextView) view.findViewById(R.id.multiplier_text)).setText(String.valueOf(multiple.getMultiplier()));
+		((TextView) view.findViewById(R.id.result_text)).setText(String.valueOf(multiple.getResult()));
 
-		return null;
+		return view;
 	}
 
 }
